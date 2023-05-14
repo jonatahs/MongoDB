7807859e2b2411d971d2dc409667d89f5be5c2ec
Jonathan Silva Sales de Oliveira <jonathansalesjobs@gmail.com>
2023-05-14 20:51:37 -0300

    mongodbConnect.py

--- mongodbConnect.py
+++ mongodbConnect.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://jonatahs18:FkodOXfGl6xGLmku@cluster0.zwytslc.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://ACCOUNT:PASSWORD@cluster0.zwytslc.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
