de7dae7aba1ca2eda86ab7959dfd2f3ca92aa211
jonatahs <jonathansalesjobs@gmail.com>
2023-06-20 15:55:05 -0700

    Delete and Update

---
+++ delete.py
@@ -0,0 +1,71 @@
+"""
+Para realizar o delete de um documento utilizamos o delete_one(), para muitos delete_many()
+
+db.collection.delete_one(<fiter>)
+
+"""
+
+from pymongo.mongo_client import MongoClient
+from bson.objectid import ObjectId
+
+# conectando no banco de dados
+client = MongoClient('mongodb+srv://jonatahs18:FkodOXfGl6xGLmku@cluster0.zwytslc.mongodb.net/')
+
+try:
+    print("\n")
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+    print("\n")
+except Exception as e:
+    print(e)
+
+# coletando referencia do Banco
+db = client.CRUD
+
+# Referencia de colecao 
+collection = db.CRUD
+
+# Filter
+
+document_to_delete = {"_id":ObjectId("64617191383aeff07aa867d1")}
+
+# realizando o delete
+
+result = collection.delete_one(document_to_delete)
+
+print("Searching for target document after delete: ")
+print(collection.find_one(document_to_delete))
+
+print("Documents deleted: " + str(result.deleted_count))
+
+
+""""
+
+DELETE MANY
+
+# Get reference to 'bank' database
+db = client.bank
+
+# Get a reference to the 'accounts' collection
+accounts_collection = db.accounts
+
+# Filter for accounts with balance less than $2000
+documents_to_delete = {"balance": {"$lt": 2000}}
+
+# Search for sample document before delete
+print("Searching for sample target document before delete: ")
+pprint.pprint(accounts_collection.find_one(documents_to_delete))
+
+# Write an expression that deletes the target accounts.
+result = accounts_collection.delete_many(documents_to_delete)
+
+# Search for sample document after delete
+print("Searching for sample target document after delete: ")
+pprint.pprint(accounts_collection.find_one(documents_to_delete))
+
+print("Documents deleted: " + str(result.deleted_count))
+
+client.close()
+
+
+"""
---
+++ update.py
@@ -0,0 +1,71 @@
+"""
+Para realizar o update de um documento utilizamos o update_one(), para muitos update_many()
+
+db.collection.update_one(<fiter>, <update>)
+
+"""
+
+from pymongo.mongo_client import MongoClient
+from bson.objectid import ObjectId
+
+# conectando no banco de dados
+client = MongoClient('mongodb+srv://jonatahs18:FkodOXfGl6xGLmku@cluster0.zwytslc.mongodb.net/')
+
+try:
+    print("\n")
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+    print("\n")
+except Exception as e:
+    print(e)
+
+
+# Realizando find de um objeto
+
+# result = client['CRUD']['CRUD'].find_one()
+
+# referencia database
+db = client.CRUD
+
+# referencia da colecao
+
+collection = db.CRUD
+
+# filter
+
+documento_to_update = {"_id": ObjectId("646170f9b9a87427f6b4a772")}
+
+# update
+
+# Acrecenta mais 15000
+add_accountID = {"$inc":{"limit":15000}}
+
+result = collection.update_one(documento_to_update, add_accountID)
+
+print("Documents updated: " + str(result.modified_count))
+
+"""
+UPDATE MANY
+
+
+# Get reference to 'bank' database
+db = client.bank
+
+# Get reference to 'accounts' collection
+accounts_collection = db.accounts
+
+# Filter
+select_accounts = {"account_type": "savings"}
+
+# Update
+set_field = {"$set": {"minimum_balance": 100}}
+
+# Write an expression that adds a 'minimum_balance' field to each savings acccount and sets its value to 100.
+result = accounts_collection.update_many(select_accounts, set_field)
+
+print("Documents matched: " + str(result.matched_count))
+print("Documents updated: " + str(result.modified_count))
+pprint.pprint(accounts_collection.find_one(select_accounts))
+
+client.close()
+"""
